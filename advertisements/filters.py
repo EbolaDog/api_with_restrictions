29898ff6a80a2a58d7619bc472cc5fa2d0a1dd99
Maxim <floidworldpro@gmail.com>
2023-12-25 09:11:54 +0300

    first commit

---
+++ advertisements/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as filters, DateFromToRangeFilter, CharFilter
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    created_at = DateFromToRangeFilter()
+    status = CharFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['status','created_at']
---
+++ advertisements/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from advertisements.models import Advertisement
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer для пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name',
+                  'last_name',)
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    """Serializer для объявления."""
+
+    creator = UserSerializer(
+        read_only=True,
+    )
+
+    class Meta:
+        model = Advertisement
+        fields = ('id', 'title', 'description', 'creator',
+                  'status', 'created_at', )
+
+    def create(self, validated_data):
+        """Метод для создания"""
+        validated_data["creator"] = self.context["request"].user
+        return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        adv = Advertisement.objects.filter(creator_id = self.context['request'].user.id, status = 'OPEN').values()
+        if adv.count() == 10 and (('status','CLOSED') not in self.context['request'].data.items()):
+            raise ValidationError('Вы превысили лимит новых объявлений. Закройте объявления и повторите попытку.')
+        return data
+
